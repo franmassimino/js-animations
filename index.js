2eeec2f659671a3f527f34037bf5812e56c2d698
franmassimino <franmassi1904@gmail.com>
2021-08-19 23:04:54 -0300

    primer commit

---
+++ index.js
@@ -0,0 +1,54 @@
+const entrar = document.getElementById('entrar')
+const entrarRight = document.getElementById('entrar-right')
+
+
+gsap.from('.overlay-title', {
+    y: -20,
+    duration: 1,
+    delay: .75,
+    opacity: 0,
+})
+
+gsap.from('#entrar', {
+    duration: .5,
+    delay: .8,
+    opacity: 0
+})
+
+entrar.addEventListener('click', (e) => {
+    e.preventDefault()
+    gsap.to('.overlay', {
+        duration: 1,
+        y: '-100%',
+        delay: .25,
+    }
+    )
+    gsap.to('.overlay-title', {
+        y: -50,
+        duration: .25,
+        opacity: 0,
+    })
+
+    gsap.to('#entrar', {
+        y: -50,
+        duration: .1,
+        opacity: 0
+    })
+
+})
+
+entrarRight.addEventListener('click', (e) => {
+    e.preventDefault()
+    gsap.to('.overlay-right', {
+        duration: 1,
+        x: 0,
+        delay: .25,
+    })
+
+    gsap.to('.hero-content', {
+        duration: 1,
+        x: '-100%',
+        delay: .25,
+    })
+
+})
